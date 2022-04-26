9fe06fe14b13a2a131aab5572e8c6a87d527d68c
celiakwan <celiavov@yahoo.com>
2022-04-27 00:04:11 +0800

    add web-service-template

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ A collection of examples for learning Rust.
 # Reference
 ### todo-list:
 - https://www.freecodecamp.org/news/how-to-build-a-to-do-app-with-rust/
+
+### web-service-template:
+- https://engineering.deptagency.com/create-a-rust-web-service-from-zero-to-cloud
+- https://actix.rs/docs/errors/
---
+++ web-service-template/src/main.rs
@@ -0,0 +1,51 @@
+use std::{env, io};
+
+use actix_files::Files;
+use actix_web::{error, get, web::Data, App, HttpResponse, HttpServer};
+use derive_more::{Display, Error};
+use handlebars::Handlebars;
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct Compliment {
+    name: &'static str,
+    adjective: &'static str,
+}
+
+#[derive(Debug, Display, Error)]
+struct HttpError {
+    message: String,
+}
+
+impl error::ResponseError for HttpError {}
+
+#[get("/")]
+async fn compliment(hb: Data<Handlebars<'_>>) -> Result<HttpResponse, HttpError> {
+    let compliment = Compliment {
+        name: "Alice",
+        adjective: "beautiful",
+    };
+    match hb.render("compliment", &compliment) {
+        Ok(r) => Ok(HttpResponse::Ok().content_type("text/html").body(r)),
+        Err(e) => Err(HttpError { message: e.desc }),
+    }
+}
+
+#[actix_web::main]
+async fn main() -> io::Result<()> {
+    let bind_address = env::var("BIND_ADDRESS").unwrap_or_else(|_| "localhost:8080".to_string());
+    let template_service = {
+        let mut handlebars = Handlebars::new();
+        handlebars
+            .register_templates_directory(".html", "web/templates")
+            .expect("Could not register templates directory");
+        Data::new(handlebars)
+    };
+    let server = move || {
+        App::new()
+            .app_data(template_service.clone())
+            .service(Files::new("/public", "web/public").show_files_listing())
+            .service(compliment)
+    };
+    HttpServer::new(server).bind(bind_address)?.run().await
+}
